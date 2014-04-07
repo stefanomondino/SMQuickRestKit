8c0c6be29700027b14b7958e7d18d43e64276130
Stefano Mondino <stefano.mondino.dev@gmail.com>
2014-04-07 10:52:19 +0200

    RestKit.h

---
+++ SMQuickRestKit/SMQuickRestKit.h
@@ -0,0 +1,14 @@
+//
+//  SMObjectRequest.h
+//  SMQuickRestKit
+//
+//  Created by Stefano Mondino on 25/10/13.
+//  Copyright (c) 2013 Stefano Mondino. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#import "SMArrayValueTransformer.h"
+#import "SMQuickObjectMapper.h"
+#import "SMSavedModel"
+#import "SMUnsavedModel"
